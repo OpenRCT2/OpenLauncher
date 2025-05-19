358d5c8166deaaf4257ecdade1c613f61cb74f69
MichaelJBerk <49624366+MichaelJBerk@users.noreply.github.com>
2025-05-19 17:55:09 -0400

    Add support for macOS
    This PR adds support for OpenLauncher on macOS. Currently the launcher builds and runs on macOS, but can't download new releases or launch the game.
    
    - OpenLauncher now uses the correct binary name on macOS so that it can launch the executable properly. 
    - On all platforms, OpenLauncher currently uses `ZipFile.ExtractToDirectory` to extract the release once it's been downloaded. However, this method will not preserve file aliases on macOS, and since `OpenRCT2.app` contains several of them, the extracted application won't launch. To fix this, this PR calls`/usr/bin/ditto` in a subprocess to extract the `.zip`, which will preserve the files properly.
    - Since OpenLoco isn't currently supported on macOS, it doesn't appear in the launcher.
    - `.gitignore` is updated to ignore `.ds_store` and other hidden files that macOS likes to make
    
    Co-authored-by: Michael Steenbeek <m.o.steenbeek@gmail.com>

---
+++ package-mac.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#Modified from Avalonia's documentation: https://docs.avaloniaui.net/docs/0.10.x/distribution-publishing/macos
+set -e
+trap 'echo "Error packaging app"; exit 1' ERR
+
+APP_NAME="./src/openlauncher/bin/Release/net8.0/macos-universal/OpenLauncher.app"
+PUBLISH_OUTPUT_DIRECTORY="./src/openlauncher/bin/Release/net8.0/macos-universal/."
+
+INFO_PLIST="./info.plist"
+
+ICON_FILE="./src/openlauncher/resources/logo-mac.icns"
+
+if [ -d "$APP_NAME" ]
+then
+    rm -rf "$APP_NAME"
+fi
+
+echo "Creating OpenLauncher.app"
+
+rm -r -f "$APP_NAME"
+mkdir "$APP_NAME"
+
+mkdir "$APP_NAME/Contents"
+mkdir "$APP_NAME/Contents/MacOS"
+mkdir "$APP_NAME/Contents/Resources"
+
+cp "$INFO_PLIST" "$APP_NAME/Contents/Info.plist"
+cp "$ICON_FILE" "$APP_NAME/Contents/Resources/AppIcon.icns"
+for FILE in "$PUBLISH_OUTPUT_DIRECTORY"/*; do
+	if [ -f "$FILE" ]; then
+		cp -a "$FILE" "$APP_NAME/Contents/MacOS/."
+	fi
+done
+
+#Get version number from csproj
+VERSION_NUMBER=$(sed -n 's/.*<AssemblyVersion>\(.*\)<\/AssemblyVersion>.*/\1/p' ./src/openlauncher/openlauncher.csproj)
+#Replace placeholder version with real version number
+sed -i -e "s/APP_VERSION_NUMBER/$VERSION_NUMBER/" "$APP_NAME/Contents/Info.plist"
+#For whatever reason, sed on macOS creates a backup file when replacing text, so we need to remove it
+rm  "$APP_NAME/Contents/Info.plist-e"
+
+codesign --sign - --force --deep "./src/openlauncher/bin/Release/net8.0/macos-universal/OpenLauncher.app"
+
+mkdir "$PUBLISH_OUTPUT_DIRECTORY/publish"
+
+echo "Zipping OpenLauncher.app..."
+
+ditto -c -k --keepParent "./src/openlauncher/bin/Release/net8.0/macos-universal/OpenLauncher.app" "./src/openlauncher/bin/Release/net8.0/macos-universal/publish/OpenLauncher.zip"
